74edeb645cc0bf141812eb20323e9708e96b5014
dgonzale333 <davidgonzalezdiaz2305@gmail.com>
2025-03-11 11:48:36 -0500

    Estructura inicial

---
+++ src/components/atomics/Li/Li.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Li.css'
+import logo from '../../../../public/vite.svg'
+
+
+export const Li = () => {
+  return (
+    <>
+
+    <div className="nav-li">
+      <div className="logo-header">
+    <img  src= {logo} alt="" />
+    <h1 >David</h1>
+    </div>
+      <ul className='ul-nav'>
+      <li><a href="" className='li-a'>Inicio</a></li>
+      <li><a href="" className='li-a'>Sobre mi</a></li>
+      <li><a href="" className='li-a'>Proyectos</a></li>
+      <li><a href="" className='li-a'>Contacto</a></li>
+      </ul>
+      
+      </div>
+    </>
+  );
+};
+
+
+
---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+import { Li } from '../atomics/Li/Li'
+
+export const Header = () => {
+  return (
+   <header>
+  
+        <ul>
+           <Li />
+        </ul>
+   </header>
+  )
+}
+
+
